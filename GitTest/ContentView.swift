7ccee56a7d602b3cc544f9934de0b219a2b17cb7
Allan Moreno <allanmoreno@Allans-MacBook-Pro.local>
2020-01-10 13:00:18 -0800

    added multiple items

--- GitTest/ContentView.swift
+++ GitTest/ContentView.swift
@@ -2,17 +2,35 @@
 //  ContentView.swift
 //  GitTest
 //
-//  Created by Carlos Varela on 1/4/20.
-//  Copyright © 2020 Carlos Varela. All rights reserved.
+//  Created by Carlos Varela, Allan Moreno  on 1/4/20.
+//  Copyright © 2020 Carlos Varela, Allan Moreno . All rights reserved.
 //
 
 import SwiftUI
+import UIKit
+
 
 struct ContentView: View {
     var body: some View {
-        Text("Rigga")
+        List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+            Image(systemName: "photo")
+            
+            VStack(alignment: .leading){
+                Text("Rooms")
+                Text("20 People")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+            
+            
+        }
+        
+        
+        
+        
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
